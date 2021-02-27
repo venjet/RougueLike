5b375bcc0547456e68c19b9aa926be342227fa8c
venjet <venjet@gmail.com>
2021-02-27 23:31:04 +0800

    上传可执行文件

---
+++ Temp/gradleOut/build/generated/source/buildConfig/release/com/anti3x/roguelike/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.anti3x.roguelike;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.anti3x.roguelike";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.9";
+}
